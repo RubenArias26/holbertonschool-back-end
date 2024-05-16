f868f4089cbe1c004e8d4f341385b73000ee4817
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-05-15 19:06:51 -0500

    I updated and debug this task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""Consumimos API para extraer información ficticia"""
+
 import requests
-from sys import argv
+import sys
+
+def main ():
+    #coloco el url del json donde vamos a sacar la informacion
+    url = "https://jsonplaceholder.typicode.com/"
+    #luego se crea una variable para que contenga el parametro de busqueda que el usuario introducira
+    id_employee = int(sys.argv[1])
+    #luego iniciamos con la peticion http
+    todos = requests.get(url + "/todos").json()
+    names = requests.get(url + "/users").json()
+    #ahora creamos la variable donde va a contener toda la informacion que obtendra , luego de la iteracion que realizemos
+    user_todos = []
+    user_names = []
+    user_tasks = [] 
+    #este bucle es para encontrar y filtrar los titles
+    for todo in todos:
+        if todo["userId"] == id_employee:
+            user_tasks.append(todo)
+            if todo["completed"] == True:
+                user_todos.append(todo["title"])
 
 
-def main():
-    """Consultamos el nombre y las tareas de un empleado."""
-    if len(argv) > 1 and argv[1].isdigit():
-        id = argv[1]
+    #este es para encontrar el nombre del empleado
+    for name in names:
+        if name["id"] == id_employee:
+            user_names.append(name["name"])
 
-        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
-        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-
-        try:
-            response = requests.get(url_id)
-
-            if response.status_code == 200:
-                data = response.json()
-                EMPLOYEE_NAME = data['name']
-
-            response = requests.get(url_todos)
-
-            if response.status_code == 200:
-                todos = response.json()
-
-                total_task = sum(1 for todo in todos if todo['completed'])
-
-                NUMBER_OF_DONE_TASKS = total_task
-                TOTAL_NUMBER_OF_TASKS = len(todos)
-                text = f"Employee {EMPLOYEE_NAME} is done with "\
-                    f"tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):"
-
-                print(text)
-                for todo in todos:
-                    if todo['completed']:
-                        print(f'\t {todo["title"]}')
-
-        except requests.exceptions.HTTPError as e:
-            print(f"Error de solictud: {e}")
-
+    print("Employee {} is done with tasks ({}/{}):".format(user_names[0], len(user_todos), len(user_tasks)))
+    for task in user_todos:
+        print("\t " + task)
 
 if __name__ == '__main__':
     main()
