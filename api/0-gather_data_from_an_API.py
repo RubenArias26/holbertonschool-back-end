7049ea13849db7d496ef24981dcc374870104d91
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-05-21 19:04:58 -0500

    I updated my code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-"""Usamos api para extraer informacion de un archivo json"""
+"""Usamos API para extraer información de un archivo JSON"""
 import requests
 from sys import argv
 
-def main():
-    #luego se crea una variable para que contenga el parametro de busqueda que el usuario introducira
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("argumento no valido, introduzca un solo argumento que sea un numero")
-        return
-    
-    id = int(argv[1])
 
+def main():
+    """Consultamos el nombre y las tareas de un empleado."""
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Argumento no válido, introduzca un solo argumento que sea un número")
+        return
+
+    id = int(argv[1])
     url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
     url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
-    # bloque de try-except con el que vamos a trabajar
     try:
-        # buscamos obtener el id que se da como argumento y guardarlo en una variable con el nombre relacionado a su id
         response = requests.get(url_id).json()
-
         if response["id"] == id:
-            data = response["name"]
-            EMPLOYEE_NAME = data
+            EMPLOYEE_NAME = response["name"]
 
         todos = requests.get(url_todos).json()
         NUMBER_OF_DONE_TASKS = []
         TOTAL_NUMBER_OF_TASKS = []
 
-        # iteramos para poder añadir todas las tasks que tiene tal id, y para obtener tambien las que ha concluido
         for todo in todos:
-            if todo["userId"] == id:
-                TOTAL_NUMBER_OF_TASKS.append(todo)
-                if todo["completed"] == True:
-                    NUMBER_OF_DONE_TASKS.append(todo["title"])
-        
-        text = f"employee {EMPLOYEE_NAME} is done with tasks({len(NUMBER_OF_DONE_TASKS)}/{len(TOTAL_NUMBER_OF_TASKS)}):"
+            TOTAL_NUMBER_OF_TASKS.append(todo)
+            if todo["completed"]:
+                NUMBER_OF_DONE_TASKS.append(todo["title"])
 
+        text = (f"Employee {EMPLOYEE_NAME} is done with "
+                f"tasks({len(NUMBER_OF_DONE_TASKS)}/{len(TOTAL_NUMBER_OF_TASKS)}):")
         print(text)
 
         for task in NUMBER_OF_DONE_TASKS:
@@ -43,5 +37,7 @@ def main():
 
     except requests.exceptions.RequestException as e:
         print(f"Error de solicitud: {e}")
+
+
 if __name__ == "__main__":
     main()
